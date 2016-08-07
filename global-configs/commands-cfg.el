da884741a21c2d1510f98c5682c940e8f96b7b09
jkahn <kahn.jms@gmail.com>
2016-08-07 13:08:17 +0200

    Updated texlive path to 2016

--- global-configs/commands-cfg.el
+++ global-configs/commands-cfg.el
@@ -5,6 +5,6 @@
 (savehist-mode 1)
 
 ;; Add texlive bin to env variable PATH
-(setenv "PATH" (concat (getenv "HOME")"/.local/texlive/2015/bin/x86_64-linux:" (getenv "PATH")))
+(setenv "PATH" (concat (getenv "HOME")"/.local/texlive/2016/bin/x86_64-linux:" (getenv "PATH")))
 ;; And add it to exec-path variable
 ;(setq exec-path (append exec-path '("~/.local/texlive/2015/bin/x86_64-linux")))
